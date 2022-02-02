c761bd736a516b018b48b11a2bacddd44647b2fe
ctola91 <ctolapacheco@gmail.com>
2022-02-02 00:24:20 -0400

    test shallowMount and mount

--- tests/unit/components/Message.spec.js
+++ tests/unit/components/Message.spec.js
@@ -1,9 +1,10 @@
 import Message from "@/components/Message";
-import { mount } from "@vue/test-utils";
+import { mount, shallowMount } from "@vue/test-utils";
 
 describe("Message.vue", () => {
   it("Verificar que el componente se renderiza correctamente", () => {
     const text = 'Hola Mundo';
+    // const wrapper = shallowMount(Message);
     const wrapper = mount(Message);
     expect(wrapper.text()).toMatch('Hola Mundo');
     expect(wrapper.html()).toMatch(`<div>${text}</div>`);
